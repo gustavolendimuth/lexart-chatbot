15aff6fff11599755525295ae9ca7a28f97978df
Gustavo Lendimuth <gustavolendimuth@gmail.com>
2023-07-15 20:11:49 -0300

    Remove node_modules

--- backend/build/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorMiddleware = void 0;
-const errorMiddleware = (err, _req, res, _next) => {
-    const { statusCode, message } = err;
-    return res.status(statusCode || 500).json({ message });
-};
-exports.errorMiddleware = errorMiddleware;
-//# sourceMappingURL=errorMiddleware.js.map
